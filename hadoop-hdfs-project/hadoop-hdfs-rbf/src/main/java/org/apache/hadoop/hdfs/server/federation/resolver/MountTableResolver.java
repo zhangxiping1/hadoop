f4a4d6c77fa9ccee19a748850fd2acc2d5b56341
章锡平 <40832063+zhangxiping1@users.noreply.github.com>
2022-11-25 16:09:20 +0800

    Bug-51540: 用户个人文件回收站目录展示文件异常

--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/resolver/MountTableResolver.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/resolver/MountTableResolver.java
@@ -511,9 +511,6 @@ public class MountTableResolver
   public List<String> getMountPoints(final String str) throws IOException {
     verifyMountTable();
     String path = RouterAdmin.normalizeFileSystemPath(str);
-    if (isTrashPath(path)) {
-      path = subtractTrashCurrentPath(path);
-    }
 
     Set<String> children = new TreeSet<>();
     readLock.lock();
